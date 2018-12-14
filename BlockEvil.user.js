c001bbe6c02cccb9c4c8560169c6cb881c7d6e74
Bryan Elliott <fordiman@gmail.com>
2018-12-13 21:01:00 -0500

    Fixes Issue #2

--- BlockEvil.user.js
+++ BlockEvil.user.js
@@ -10,28 +10,46 @@
 // ==/UserScript==
 
 (function _i_am_root() {
-    'use strict';
-    const noEval = () => {
+    const isEval = () => {
         const stack = new Error().stack.split('\n').slice(3).join('\n');
-        if (
-            stack.split('\n').length > 1 && (
-                // eval'd script
-                stack.indexOf('eval') !== -1
-                // Inserted script with data source
-                || /data:[^\/]+\/[^\/]+;/.test(stack)
-                // Inserted script with blob source
-                || stack.indexOf('blob:') !== -1
-                // Chrome's trace for an inserted inline script
-                || stack.indexOf('<anonymous>:1:1') !== -1
-                // Line 1 on an HTML page is the trace in Firefox for an inserted inline script
-                || stack.indexOf(`@${location}:1`) !== -1
-            )
-        ) {
+        return (
+            // eval'd script
+            stack.indexOf('eval') !== -1
+            // Inserted script with data source
+            || /data:[^\/]+\/[^\/]+;/.test(stack)
+            // Inserted script with blob source
+            || stack.indexOf('blob:') !== -1
+            // Chrome's trace for an inserted inline script
+            || stack.indexOf('<anonymous>:1:1') !== -1
+            // Line 1 on an HTML page is the trace in Firefox for an inserted inline script
+            || stack.indexOf(`@${location}:1`) !== -1
+        );
+    };
+    const noEval = () => {
+        if (isEval()) {
             var err = new Error('I control my browser, not you.  Stop trying.');
             err.stack = '';
             throw err;
         }
     };
+    const wrapCallback = (callback, acceptString) => {
+        if (acceptString && typeof callback === 'string') {
+            callback = new Function(callback);
+        } else if (isEval()) {
+            const ofn = callback;
+            callback = function eval() {
+                ofn();
+            };
+        }
+        return callback;
+    };
+    const sto = window.setTimeout;
+    window.setTimeout = (fn, time) => sto(wrapCallback(fn, true), time);
+    const si = window.setInterval;
+    window.setInterval = (fn, time) => si(wrapCallback(fn, true), time);
+    const raf = window.requestAnimationFrame;
+    window.requestAnimationFrame = (fn) => raf(wrapCallback(fn));
+
     const noEvalFor = (obj, name) => {
         const real = obj[name];
         const get = () => {
